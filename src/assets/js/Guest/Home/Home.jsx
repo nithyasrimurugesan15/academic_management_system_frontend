a85b3b2d10dd890a20296c13f25414ebc333c980
nithyasrimurugesan15 <amnithyasrimurugesan@gmail.com>
2026-04-07 13:55:48 +0530

    fix final random quote naming

--- src/assets/js/Guest/Home/Home.jsx
+++ src/assets/js/Guest/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Slider from "./slider/Slider";
-import RandomQuote from "../../components/random-quote/RanDomQuote";
+import RandomQuote from "../../components/random-quote/RandomQuote";
 import SummaryData from "./summary-data/SummaryData";
 import WhatOffer from "./what-offer/WhatOffer";
 import Footer from "../../components/Footer/Footer";
